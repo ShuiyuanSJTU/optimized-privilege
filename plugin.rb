d9e5d0962c4568385d56916a7a543196a78b01ab
dujiajun <cqdujiajun@outlook.com>
2021-05-31 08:02:46 +0000

    remove unused settings

--- plugin.rb
+++ plugin.rb
@@ -19,72 +19,76 @@ load File.expand_path('lib/optimized-privilege/engine.rb', __dir__)
 after_initialize do
   # https://github.com/discourse/discourse/blob/master/lib/plugin/instance.rb
 
-  class ::TopicViewDetailsSerializer
-    module OverridingTopicViewDetailsSerializer
-      def include_can_close_topic? 
-        scope.can_close_topic?(object.topic)
-      end
-      alias :include_can_split_merge_topic? :include_can_close_topic?
-      alias :include_can_archive_topic? :include_can_close_topic?
-    end
-    prepend OverridingTopicViewDetailsSerializer
-  end
-  
-  class ::Guardian
-    module OverridingGuardian
+  if SiteSetting.optimized_privilege_enabled?
 
-      def can_delete_post?(post)
-        return true if super(post)
-        # You can delete your own posts
-        if is_my_own?(post)
-          return false if (SiteSetting.max_post_deletions_per_minute < 1 || SiteSetting.max_post_deletions_per_day < 1)
-          return true if !post.user_deleted?
+    class ::TopicViewDetailsSerializer
+      module OverridingTopicViewDetailsSerializer
+        def include_can_close_topic? 
+          scope.can_close_topic?(object.topic)
         end
-    
-        false
+        alias :include_can_split_merge_topic? :include_can_close_topic?
+        alias :include_can_archive_topic? :include_can_close_topic?
       end
-     
-      def can_delete_topic?(topic)
-        return true if super(topic)
-        return true if is_my_own?(topic)
-        false
-      end
-
-      def can_close_topic?(topic)
-        return true if super(topic)
-        return true if is_my_own?(topic)
-        false
-      end
-
-      def can_toggle_topic_visibility?(topic)
-        return true if super(topic)
-        return true if is_my_own?(topic)
-        false
-      end
-
-      def can_move_posts?(topic)
-        return true if super(topic)
-        return true if is_my_own?(topic)
-        false
-      end
-
-      alias :can_archive_topic? :can_close_topic?
-      alias :can_split_merge_topic? :can_close_topic?
-      alias :can_open_topic? :can_close_topic?
-
-      def can_edit_username?(user)
-        return false if SiteSetting.auth_overrides_username?
-        return true if is_staff?
-        return false if is_anonymous?
-        is_me?(user)
-      end
-
+      prepend OverridingTopicViewDetailsSerializer
     end
     
-    prepend OverridingGuardian
-
-  end
+    class ::Guardian
+      module OverridingGuardian
   
+        def can_delete_post?(post)
+          return true if super(post)
+          # You can delete your own posts
+          if is_my_own?(post)
+            return false if (SiteSetting.max_post_deletions_per_minute < 1 || SiteSetting.max_post_deletions_per_day < 1)
+            return true if !post.user_deleted?
+          end
+      
+          false
+        end
+       
+        def can_delete_topic?(topic)
+          return true if super(topic)
+          return true if is_my_own?(topic)
+          false
+        end
+  
+        def can_close_topic?(topic)
+          return true if super(topic)
+          return true if is_my_own?(topic)
+          false
+        end
+  
+        def can_toggle_topic_visibility?(topic)
+          return true if super(topic)
+          return true if is_my_own?(topic)
+          false
+        end
+  
+        def can_move_posts?(topic)
+          return true if super(topic)
+          return true if is_my_own?(topic)
+          false
+        end
+  
+        alias :can_archive_topic? :can_close_topic?
+        alias :can_split_merge_topic? :can_close_topic?
+        alias :can_open_topic? :can_close_topic?
+  
+        def can_edit_username?(user)
+          return false if SiteSetting.auth_overrides_username?
+          return true if is_staff?
+          return false if is_anonymous?
+          is_me?(user)
+        end
+  
+      end
+      
+      prepend OverridingGuardian
+  
+    end
+    
+  end
+
 
 
 end
