e486c4f4b797363bdb89cedf732f86c4322a9219
Jiajun Du <jiajun_du@outlook.com>
2022-05-16 01:53:17 +0000

    refactor to use custom_fields

--- plugin.rb
+++ plugin.rb
@@ -12,70 +12,89 @@ enabled_site_setting :optimized_privilege_enabled
 
 PLUGIN_NAME ||= 'OptimizedPrivilege'
 
+
 after_initialize do
-  if SiteSetting.optimized_privilege_enabled
-    module OverridingTopicViewDetailsSerializer
-      def include_can_archive_topic?
-        scope.can_archive_topic?(object.topic)
+
+  Topic.register_custom_field_type 'closed_by', :integer
+  User.register_custom_field_type 'last_changed_username', :datetime
+
+  module OverridingTopic
+    def update_status(status, enabled, user, opts = {})
+      super
+      if status == 'closed' && enabled
+        self.custom_fields['closed_by'] = user.id
+        self.save!
+      end
+    end
+  end
+
+  class ::Topic
+    prepend OverridingTopic
+  end
+
+  module OverridingTopicViewDetailsSerializer
+    def include_can_close_topic?
+      scope.can_close_topic?(object.topic)
+    end
+  end
+
+  class ::TopicViewDetailsSerializer
+    prepend OverridingTopicViewDetailsSerializer
+  end
+
+  module OverridingTopicGuardian
+    def can_close_topic?(topic)
+      if SiteSetting.optimized_can_close_topic
+        return true if super
+        if !topic.closed || (topic.closed && topic.custom_fields['closed_by'] == @user&.id)
+          # 只能打开自己关的
+          return true 
+        end
+          false
+      else super
+      end
+    end
+  end
+
+  module OverrideUserGuardian
+    def can_edit_username?(user)
+      if SiteSetting.optimized_change_username
+        return false if SiteSetting.auth_overrides_username?
+          return true if is_staff?
+          return false if is_anonymous?
+          is_me?(user)
+      else super
+      end
+      end
+  end
+
+  class ::Guardian
+    prepend OverridingTopicGuardian
+    prepend OverrideUserGuardian
+  end
+
+  class ::UsersController
+    before_action :check_change_username_limit, only: [:username]
+    after_action :add_change_username_limit, only: [:username]
+    def check_change_username_limit
+      if SiteSetting.optimized_change_username && !current_user&.staff?
+        old = current_user.custom_fields['last_changed_username']
+        if old
+          time = Time.parse(old) + SiteSetting.optimized_username_change_period * 86400
+            if Time.now < time
+              render json: { success: false, message: "正处于更名间隔期，请于#{time.strftime("%Y-%m-%d %H:%M:%S %Z")}之后再尝试！" }, status: 403
+            end
+        end
       end
     end
 
-      class ::TopicViewDetailsSerializer
-        prepend OverridingTopicViewDetailsSerializer
+    def add_change_username_limit
+      if SiteSetting.optimized_change_username
+        current_user.custom_fields['last_changed_username'] = Time.now
+        current_user.save!
       end
-
-      module OverridingTopicGuardian
-        def can_archive_topic?(topic)
-          if SiteSetting.optimized_can_close_topic
-            return true if super
-              return true if is_my_own?(topic)
-              false
-          else super
-          end
-        end
-      end
-
-      module OverrideUserGuardian
-        def can_edit_username?(user)
-          if SiteSetting.optimized_change_username
-            return false if SiteSetting.auth_overrides_username?
-              return true if is_staff?
-              return false if is_anonymous?
-              is_me?(user)
-          else super
-          end
-          end
-      end
-
-      class ::Guardian
-        prepend OverridingTopicGuardian
-        prepend OverrideUserGuardian
-      end
-
-      class ::UsersController
-        before_action :check_change_username_limit, only: [:username]
-        after_action :add_change_username_limit, only: [:username]
-        def check_change_username_limit
-          if SiteSetting.optimized_change_username && !current_user&.staff?
-            params.require(:username)
-              old = ::PluginStore.get("change-username", params[:username].downcase)
-              if old
-                time = Time.parse(old) + SiteSetting.optimized_username_change_period * 86400
-                  if Time.now < time
-                    render json: { success: false, message: "正处于更名间隔期，请于#{time.strftime("%Y-%m-%d %H:%M:%S %Z")}之后再尝试！" }, status: 403
-                  end
-              end
-          end
-        end
-
-        def add_change_username_limit
-          if SiteSetting.optimized_change_username
-            ::PluginStore.remove("change-username", params[:username].downcase)
-              ::PluginStore.set("change-username", params[:new_username].downcase, Time.now)
-          end
-        end
-      end
-
+    end
   end
+
 
 end
